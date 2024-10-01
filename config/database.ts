83169c6a875212074d70365e7279b575bd37fe75
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-10-02 01:49:44 +0700

    1.4. Kết nối database

---
+++ config/database.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME, // Tên database
+  process.env.DATABASE_USERNAME, // username
+  process.env.DATABASE_PASSWORD, // password
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql'
+  }
+);
+sequelize.authenticate().then(() => {
+   console.log('Kết nối Database thành công.');
+}).catch((error) => {
+   console.error('Kết nối Database thất bại: ', error);
+});
+export default sequelize;
