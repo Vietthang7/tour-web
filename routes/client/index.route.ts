8cf0ac1f5e6a2d1072b25eb79e3bb49a7c5ae984
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-10-04 20:01:29 +0700

    3. Làm trang Danh mục tour (bên client)

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,5 +1,7 @@
 import { Express } from "express";
 import { tourRoute } from "./tour.route";
+import { categoryRoute } from "./category.route";
 export const routesClient = (app: Express) => {
   app.use("/tours", tourRoute);
+  app.use("/categories", categoryRoute);
 }
