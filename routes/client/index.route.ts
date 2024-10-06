19dbe3c967637ab64b2cfb2cd3b560aad32c3e3e
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-10-06 16:07:13 +0700

    2.1. Lấy thông tin đơn hàng gửi lên server

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,8 +2,10 @@ import { Express } from "express";
 import { tourRoute } from "./tour.route";
 import { categoryRoute } from "./category.route";
 import { cartRoute } from "./cart.route";
+import { orderRoute } from "./order.route";
 export const routesClient = (app: Express) => {
   app.use("/tours", tourRoute);
   app.use("/categories", categoryRoute);
   app.use("/cart", cartRoute);
+  app.use("/order", orderRoute);
 }
