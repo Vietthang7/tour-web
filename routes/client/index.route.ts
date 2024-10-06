c59807dfb6027b3c9af9d6abdf4bb1e98b3c0333
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-10-06 14:42:23 +0700

    3.1. Làm giao diện

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import { tourRoute } from "./tour.route";
 import { categoryRoute } from "./category.route";
+import { cartRoute } from "./cart.route";
 export const routesClient = (app: Express) => {
   app.use("/tours", tourRoute);
   app.use("/categories", categoryRoute);
+  app.use("/cart", cartRoute);
 }
