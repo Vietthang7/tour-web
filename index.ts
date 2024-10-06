787dc700de29f8f322d9ddcb0138b31179b425a7
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-10-06 15:35:55 +0700

    3.2. Lấy data từ database trả ra giao diện

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import sequelize from "./config/database";
 sequelize;
+import bodyParser from "body-parser";
 import { routesClient } from "./routes/client/index.route";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
 routesClient(app);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
